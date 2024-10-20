7a5ed81164c31fcb967394f717c765c05b7517db
Migas77 <miguel.belchior@ua.pt>
2024-10-20 12:58:47 +0100

    mid setup

---
+++ main.py
@@ -0,0 +1,47 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from starlette import status
+
+import models
+from database import engine
+from routers import checkout
+
+
+@asynccontextmanager
+async def lifespan(app):
+    # async stuff here
+    # for later
+    yield
+
+models.Base.metadata.create_all(bind=engine)
+app = FastAPI(
+    lifespan=lifespan,
+    title="ClubSync Payments_Microservice API",
+    version="0.0.1",
+    docs_url="/docs",
+    redoc_url="/redoc",
+    openapi_url="/openapi.json",
+    contact={
+        "name": "ClubSync",
+    },
+    servers=[{"url": "http://localhost:8000", "description": "Local server"}],
+)
+
+# CORS setup
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/health", tags=["healthcheck"], summary="Perform a Health Check",
+         response_description="Return HTTP Status Code 200 (OK)", status_code=status.HTTP_200_OK)
+def get_health():
+    return {"status": "ok"}
+
+
+app.include_router(checkout.router, tags=["Client"])
